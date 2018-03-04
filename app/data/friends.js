e2ab3184f6d4a196becec1c8d98a5e74b42b8b7a
Alvin Go <alvingogo25@gmail.com>
2018-03-03 17:50:08 -0800

    home and server page, friends list

---
+++ app/data/friends.js
@@ -0,0 +1,22 @@
+var friends = [
+  {
+    "name": "Lilo",
+    "photo": "http://images.clipartpanda.com/lilo-clip-art-cliplilo3.gif",
+    "scores": [4, 4, 5, 4, 1, 5, 1, 4, 5, 2]
+  },
+  {
+    "name": "Stitch",
+    "photo": "https://stickershop.line-scdn.net//stickershop/v1/product/2241/iphone/main@2x.png",
+    "scores": [1, 5, 4, 5, 5, 4, 5, 3, 4, 1]
+  },
+  {
+    "name": "Nani",
+    "photo": "http://www.behindthevoiceactors.com/_img/chars/nani-pelekai-lilo-and-stitch-90.1.jpg",
+    "scores": [3, 4, 3, 2, 2, 4, 3, 1, 3, 2]
+  },
+  {
+    "name": "David",
+    "photo": "https://www.disneyclips.com/imagesnewb/images/davidhawena.gif",
+    "scores": [4, 1, 4, 1, 2, 3, 2, 5, 1, 5]
+  }
+];
