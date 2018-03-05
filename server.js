44b80c4c249628b38c91aeef7eb24be7e43417f4
Alvin Go <alvingogo25@gmail.com>
2018-03-04 20:03:52 -0800

    fixed routing

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ var PORT = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-var apiRoutes = require("./routing/apiRoutes.js");
-var htmlRoutes = require("./routing/htmlRoutes.js");
+var apiRoutes = require("./app/routing/apiRoutes.js");
+var htmlRoutes = require("./app/routing/htmlRoutes.js");
 app.use(htmlRoutes);
 app.use(apiRoutes);
 
